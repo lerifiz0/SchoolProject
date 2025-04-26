d7be4914360b50c49dff592b31b035eef5d7c3a2
lerifiz0 <lerifiz0@gmail.com>
2025-04-26 08:48:07 +0000

    Add 36.php

---
+++ src/36.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize variables
+$student_id = 10;
+$subject = "Math";
+
+// Perform some operations
+if ($student_id == 1) {
+    echo "Hello, $subject! ($student_id)";
+} else {
+    echo "Welcome, $subject!";
+}
+?>
