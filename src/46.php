9af7a6630693e06654978ba75e1ae4938b90c719
lerifiz0 <lerifiz0@gmail.com>
2025-05-26 01:38:57 +0000

    Add 46.php

---
+++ src/46.php
@@ -0,0 +1,18 @@
+<?php
+// Simple PHP script to demonstrate basic functions and variables.
+// Here we will use basic arithmetic operations.
+$number1 = 5;
+$number2 = 3;
+$result = $number1 + $number2; // Addition
+
+// Output the result.
+echo "The sum of $number1 and $number2 is: " . $result;
+
+// Simple PHP script to demonstrate basic functions and variables.
+// Here we will use basic arithmetic operations.
+$number1 = 5;
+$number2 = 3;
+$result = $number1 + $number2; // Addition
+
+// Output the result.
+echo "The sum of $number1 and $number2 is: " . $result;
